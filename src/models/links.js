48f5452ae6d14244ba8ad023e0ba78d3efc6f2af
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-02-28 11:31:22 -0500

    agregando funcionalidad de funciones testeadas (falta openFile - recursividad para lectura de directorios)

--- src/models/links.js
+++ src/models/links.js
@@ -1,39 +1,64 @@
 let paths = require('path');
 let fs = require('fs');
+let marked = require('marked');
+let jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 export const evaluatePath = (path) => paths.isAbsolute(path);
-// console.log('evaluatePath: ' + evaluatePath('C:/Users/heidy/Desktop/LIM008-fe-md-links/src/index.js'))
-export const transformToAbsPath = (path) => paths.resolve(path);
-// console.log('transformToAbsPath: ' + transformToAbsPath('../index.js'))
+// console.log(`evaluatePath: ${evaluatePath('C:/Users/heidy/Desktop/LIM008-fe-md-links/src/index.js')}`)
+export const transformToAbsPath = (path) => paths.resolve(path); 
+// `${process.cwd(path)}/${paths.basename(path)}`; 
+// console.log(`transformToAbsPath: ${transformToAbsPath('../index.js')}`);
 export const recognizeIfIsFile = (pathAbs) => fs.statSync(pathAbs).isFile();
+// console.log(`recognizeIfIsFile: ${recognizeIfIsFile('C:/Users/Usuario/Desktop/LIM008-fe-md-links/')}`);
+export const validateExtMD = (pathAbs) => paths.extname(pathAbs);
+// console.log(`validateExtMD: ${validateExtMD('C:/Users/Usuario/Desktop/LIM008-fe-md-links/index.js')}`);
+export const getMDContent = (pathAbs) => fs.readFileSync(pathAbs, 'utf8');
+// console.log(getMDContent('C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/testDir/a/a1/a1.md'));
+export const convertMDToHtml = (contMD) => marked(contMD);
+// console.log(`convertMDToHtml: ${convertMDToHtml('[GitHub](http://github.com')}`);
+export const extractATagAttr = (contHTML, pathAbs) => {
+    const dom = new JSDOM(contHTML);
+    const text = dom.window.document.querySelector('a').textContent;
+    const link = dom.window.document.querySelector('a').href;
+    const path = pathAbs;  
+    const objInfLinks = {
+        href: link,
+        text: text,
+        file: path
+    };
+    return objInfLinks;
+};
+// console.log(const objInfLinks = extractATagAttr('<p><a href="http://github.com">texto de ruta</a></p>'));
 
-export const getFiles = (pathAbs) => {
-    if (pathAbs) {
-        const arrPath = ['ruta1','ruta2'];
-        return arrPath;
-    }
-};
-export const getMDContent = (pathAbs) => {
-    if (pathAbs) {
-        const contMD = 'contenido MD';
-        return contMD;
-    }
-};
-export const convertMDToHtml = (contMD) => {
-    if (contMD) {
-        const contHTML = 'contenido HTML';
-        return contHTML;
-    }
-};
-export const extractATagAttr = (contHTML) => {
-    if (contHTML) {
-        const objInfLinks = {
-            href:'link',
-            text:'text',
-            file:'path'
-        };
-        return objInfLinks;
-    }
-};
 export const createArrLinkObj = (objInfLinks) => {
-    if (objInfLinks) return [{href:'link',text:'text',file:'path'}];
+    const arrObjInfLinks = [];
+    arrObjInfLinks.push(objInfLinks);
+    return arrObjInfLinks;
 };
+// export const openFile = (path) => fs.openSync(path, 'rs');
+// console.log(openFile('C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/testDir'));
+// const path = 'C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/testDir/a/a1';
+// const ContentPath = fs.readdirSync(path); 
+// const pathEachChildrenDir = [];
+// const pathEachChildrenFile = [];
+// let pathEachChildren = '';
+// console.log(ContentPath);
+// ContentPath.forEach((element) => {
+//     pathEachChildren = paths.normalize(path + '/' + element);
+//     if (fs.statSync(pathEachChildren).isFile()) console.log('entra aqui'); 
+    // pathEachChildrenFile.push(pathEachChildren);
+//    else if (fs.statSync(pathEachChildren).isDirectory()) pathEachChildrenDir.push(pathEachChildren);
+// });
+// console.log(pathEachChildrenFile);
+// console.log(pathEachChildrenDir);
+
+
+
+    // children: Fs.readdirSync(dir).map(file => {
+    //     const path = Path.join(dir, file);
+    //     return Fs.lstatSync(path).isDirectory()
+    //     ? traverseSync(path)
+    //     : [path];
+    //     })
+    //   });
+    // fs.readdirSync (path [, opciones]) // leer una ruta  
