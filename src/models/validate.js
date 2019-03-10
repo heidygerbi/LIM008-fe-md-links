1d1c20212fc45b4c6e832dd7a311c96b47e85f1e
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-03-10 01:22:00 -0500

    función para validar - funcional

--- src/models/validate.js
+++ src/models/validate.js
@@ -1,46 +1,24 @@
-import { fetch } from 'node-fetch';
+import fetch from 'node-fetch';
 
-// export const extractHref = (arrObjInfLinks) => {
-//   const arrHref = [];
-//   arrObjInfLinks.forEach(element => {
-//     arrHref.push(element.href);     
-//   });
-//   return arrHref;
-// };
+export const updateArrObjLinks = (arrObjInfLinks) => new Promise((resolve) =>
+  resolve(Promise.all(arrObjInfLinks.map((element => verifyLink(element))))));
 
-// Esta función queda sin efecto pues se puede pasar el arr de objeto directo
-
-export const verifyLink = (arrObjInfLinks) => {
-  // if (arrHref) {
-  //   const arrStatus = ['status', 'ok'];
-  //   return arrStatus;
-  // }
-  return arrObjInfLinks.forEach(element => {
-    fetch(element.href)
-      .then(resul => {
-        if (resul.status >= 200 && resul.status < 300) {
-          element.status = resul.status;
-          element.value = 'OK';
-          resolve(element);
-        }
-      }).catch(err => {
-        if (resul.status < 200 || resul.status >= 300) {
-          element.status = err.code;
-          element.value = 'Fail';
-          resolve(element);
-        }
-      });
-  });
-};
-// http . cats 
-//httpstatus.com
-
-
-// export const addVerification = (objStatus) => new Promise((resolve, reject) => {
-//   // if (arrStatus) {
-//   //   const arrLinkStatus = ['link', 'status', 'ok'];
-//   //   return arrLinkStatus;
-//   // }
-//   const arrLinkStatus = objStatus.map(element => verifyLink(element));
-//   resolve(Promise.all(arrLinkStatus));
-// });
+export const verifyLink = (objInfLink) =>
+  new Promise((resolve) => fetch(objInfLink.href)
+    .then(resul => {
+      if (resul.status >= 200 && resul.status < 400) {
+        objInfLink.status = resul.status;
+        objInfLink.value = 'OK';
+        resolve(objInfLink);
+      } else if (resul.status < 200 || resul.status >= 400) {
+        objInfLink.status = resul.status;
+        objInfLink.value = 'Fail';
+        resolve(objInfLink);
+      }
+    })
+    .catch(err => {
+      objInfLink.status = 404; 
+      // err.code; // err.status
+      objInfLink.value = 'Fail';
+      resolve(objInfLink);
+    }));
