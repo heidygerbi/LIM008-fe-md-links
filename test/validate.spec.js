dd207097b5e23f193dc9e45fa0e4fb182fb64687
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-03-10 01:23:47 -0500

    test de validate.js - funcional

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,75 +1,56 @@
-import {
-  extractHref,
-  verifyLink,
-  addVerification
-} from '../src/models/validate.js';
-
-// describe('extractHref', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof extractHref).toBe('function');
-//   });
-//   it('debería retornar un array', () => {
-//     expect(typeof extractHref([{ href: 'link', text: 'text', file: 'path' }])).toBe('object');
-//   });
-//   it('debería retornar un array con todos los links encontrados en el array de objetos', () => {
-//     expect(extractHref(
-//       [{
-//         href: 'http://github.com/', 
-//         text: 'texto de ruta', 
-//         file: 'C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/index.spec.js'
-//       }, 
-//       {
-//         href: 'http://github1.com/', 
-//         text: 'texto de ruta 1', 
-//         file: 'C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/index.spec.js'
-//       }, 
-//       { 
-//         href: 'http://github2.com/', 
-//         text: 'texto de ruta 2', 
-//         file: 'C:/Users/Usuario/Desktop/LIM008-fe-md-links/test/index.spec.js'
-//       }])).toEqual(
-//       [
-//         'http://github.com/',
-//         'http://github1.com/',
-//         'http://github2.com/'
-//       ] 
-//     );
-//   });
-// });
-
-// describe('verifyLink', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof verifyLink).toBe('function');
-//   });
-//   it('debería retornar un array', () => {
-//     expect(typeof verifyLink(['href1', 'href2', 'href3'])).toBe('object');
-//   });
-//   it('debería retornar un array con dos atributos mas a partir de', () => {
-//     expect(verifyLink(['href1', 'href2', 'href3'])).toBe('object');
-//   });
-// });
-
-test('debería retornar un array de objetos anexando status y value', (done) => {
-  addVerification([{
+let paths = require('path');
+import { verifyLink, updateArrObjLinks } from '../src/models/validate.js';
+const inputArrObj = [
+  {
     text: 'GitHub',
-    href: 'http://github.com/',
-    file: paths.normalize(`${__dirname}/testDir/a/a1/a1.md`)
-  }]).then((result) => {
-    expect(result).toEqual([{
-      text: 'GitHub',
-      href: 'http://github.com/',
-      file: paths.normalize(`${__dirname}/testDir/a/a1/a1.md`),
-      status: 300,
-      value: OK
-    }]);
-    done();
-  });
+    href: 'https://github.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a1.md'))
+  },
+  {
+    text: 'GitHub',
+    href: 'http://github11.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a11/a11.md'))
+  },
+  {
+    text: 'GitHub',
+    href: 'http://github11.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/b/a11.md'))
+  }
+];
+const outputArrObj = [
+  {
+    text: 'GitHub',
+    href: 'https://github.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a1.md')),
+    status: 200,
+    value: 'OK'
+  },
+  {
+    text: 'GitHub',
+    href: 'http://github11.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a11/a11.md')),
+    status: 404,
+    value: 'Fail'
+  },
+  {
+    text: 'GitHub',
+    href: 'http://github11.com/',
+    file: paths.normalize(paths.join(__dirname, '/testDir/b/a11.md')),
+    status: 404,
+    value: 'Fail'
+  }
+];
+test('debería retornar un objeto con 2 propiedades extras', (done) => {
+  verifyLink({ text: 'GitHub', href: 'http://github.com/', file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a1.md')) })
+    .then((result) => {
+      expect(result).toEqual({ text: 'GitHub', href: 'http://github.com/', file: paths.normalize(paths.join(__dirname, '/testDir/a/a1/a1.md')), status: 200, value: 'OK'});
+      done();
+    });
 });
-// describe('addVerification', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof addVerification).toBe('function');
-//   });
-//   it('debería retornar un array', () => {
-//     expect(typeof addVerification(['href1', 'href2', 'href3'])).toBe('object');
-//   });
-// });
+test('debería retornar un array de objetos con dos propiedades adicionales por objeto: status y value', (done) => {
+  updateArrObjLinks(inputArrObj)
+    .then((result) => {
+      expect(result).toEqual(outputArrObj);
+      done();
+    });
+});
