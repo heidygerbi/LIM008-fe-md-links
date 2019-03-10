23a0a75aef9f345e5e4629f4c7356689e773ee43
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-03-10 01:20:06 -0500

    implementando validate

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 import {
-  evaluatePath,
-  transformToAbsPath,
-  recognizeIfIsFile,
-  getMDContent,
-  convertMDToHtml,
   extractATagAttr,
-  validateExtMD,
   getFile
 } from './models/links.js';
-import {
-  extractHref,
-  verifyLink,
-  addVerification
-} from '../src/models/validate.js';
+import {updateArrObjLinks} from '../src/models/validate.js';
 import { calculateStats } from '../src/models/stats.js';
 import { exists, promises } from 'fs';
-
+import marked from 'marked';
+import paths from 'path';
+import fs from 'fs';
+let options = {
+  validate: false,
+  stats: false,
+};
 export const mdLinks = (path, options) => {
   let pathAbs = path;
-  if (!evaluatePath(path)) pathAbs = transformToAbsPath(path);
-  return new Promise((resolve, reject) => {
-    if (options === undefined) {
+  if (!paths.isAbsolute(path)) pathAbs = paths.resolve(path);
+  return new Promise((resolve) => {
+    if (options === undefined || (!options.validate && !options.stats)) {
       getArrObjLinks(pathAbs)
+        .then(response => resolve(response))
+        .catch(console.error);
+    } else if (options.validate && !options.stats) {
+      getArrObjValidate(pathAbs)
         .then(response => resolve(response))
         .catch(console.error);
     }
   });
 };
-export const getArrObjLinks = (pathAbs) => new Promise((resolve, reject) => {
+export const getArrObjLinks = (pathAbs) => new Promise((resolve) => {
   let contHTML = '';
-  if (recognizeIfIsFile(pathAbs)) {
-    if (validateExtMD(pathAbs)) {
-      contHTML = convertMDToHtml(getMDContent(pathAbs));
+  if (fs.statSync(pathAbs).isFile()) {
+    if (paths.extname(pathAbs)) {
+      contHTML = marked(fs.readFileSync(pathAbs, 'utf8'));
       resolve(extractATagAttr(contHTML, pathAbs));
     }
   } else {
     let arrLinks = [];
     getFile(pathAbs).forEach(element => {
-      if (validateExtMD(element)) {
-        contHTML = convertMDToHtml(getMDContent(element));
+      if (paths.extname(element)) {
+        contHTML = marked(fs.readFileSync(element, 'utf8'));
         arrLinks = arrLinks.concat(extractATagAttr(contHTML, element));
       }
     });
@@ -46,3 +46,12 @@ export const getArrObjLinks = (pathAbs) => new Promise((resolve, reject) => {
   }
 });
 
+export const getArrObjValidate = (pathAbs) => new Promise((resolve) => {
+  const arrObjLinks = getArrObjLinks(pathAbs)
+    .then(response => resolve(response))
+    .catch(console.error);
+  const result = updateArrObjLinks(arrObjLinks)
+    .then(response => resolve(response))
+    .catch(console.error);
+  resolve(result);
+});
