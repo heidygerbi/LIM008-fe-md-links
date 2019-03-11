03c255d2dc633072cafc719d717968ca927b9c0f
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-03-11 11:23:42 -0500

    agregando funciones para estadísticas

--- src/models/stats.js
+++ src/models/stats.js
@@ -1,6 +1,31 @@
-export const calculateStats = (arrObjComplet) => {
-  if (arrObjComplet) {
-    const arrStats = ['Stats', 'unique', 'broken'];
-    return arrStats;
+export const calculateStats = (arrObjLinks) => {
+  if (arrObjLinks[0].status && arrObjLinks[0].value) {
+    const stats = {
+      total: arrObjLinks.length,
+      unique: getArrLinksUnique(arrObjLinks).length,
+      broken: getArrLinksBroken(arrObjLinks).length
+    };
+    return stats;
+  } else {
+    const stats = {
+      total: arrObjLinks.length,
+      unique: getArrLinksUnique(arrObjLinks).length
+    };
+    return stats;
   }
+};
+
+export const getArrLinksUnique = (arrObjLinks) => {
+  const arrHref = [];
+  arrObjLinks.forEach(element => arrHref.push(element.href));
+  return arrHref.filter((value, index, result) => result.indexOf(value) === index);
+};
+
+export const getArrLinksBroken = (arrObjLinks) => {
+  const arrBroken = [];
+  arrObjLinks.forEach(element => {
+    if (element.status < 200 || element.status >= 400)
+      arrBroken.push(element.status);
+  });
+  return arrBroken;
 };
